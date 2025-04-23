80cea02fb6b7fb6a052d7e1dd2305fe537e22a08
Mickael <mickaelgoliveira@gmail.com>
2025-04-23 12:34:36 -0300

    feat: create base modules, services, and schemas for app

--- src/modules/books/books.service.ts
+++ src/modules/books/books.service.ts
@@ -4,14 +4,36 @@ import { CreateBookDto } from "./dto/create-book.dto";
 
 @Injectable()
 export class BooksService {
-    constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
-    async create(createBookDto: CreateBookDto) {
-        return this.prisma.book.create({
-            data: createBookDto,
-            include: {
-                author: true,
-            },
-        });
+  async create(createBookDto: CreateBookDto) {
+    let authorId: CreateBookDto.authorId;
+
+    if (!authorId && createBookDto.authorName) {
+      const author = await this.prisma.author.findFirst({
+        where: {
+          name: createBookDto.authorName,
+        },
+      });
+      if (!author) {
+        throw new NotFoundException("Autor não encontrado");
+      }
+      authorId = author.id;
     }
+
+    if (!authorId && !createBookDto.authorId) {
+      throw new BadRequestException("Autor não encontrado");
+    }
+    const { authorName, ...bookData } = createBookDto;
+
+    return this.prisma.book.create({
+      data: {
+        ...bookData,
+        authorId,
+      },
+      include: {
+        author: true, // Inclui os dados do autor na resposta
+      },
+    });
+  }
 }
--- src/modules/books/dto/create-book.dto.ts
+++ src/modules/books/dto/create-book.dto.ts
@@ -1,39 +1,48 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsInt, IsNotEmpty, IsString, Min } from "class-validator";
+import { IsInt, IsNotEmpty, IsOptional, IsString, Min } from "class-validator";
 
 export class CreateBookDto {
   @IsString()
   @IsNotEmpty()
   @ApiProperty({
-    description: ' Titulo do livro',
+    description: " Titulo do livro",
   })
   title: string;
 
   @IsString()
   @IsNotEmpty()
   @ApiProperty({
-    description: 'ISBN do livro',
+    description: "ISBN do livro",
   })
   isbn: string;
 
   @IsInt()
   @Min(1900)
   @ApiProperty({
-    description: ' Ano de publicação do livro',
+    description: " Ano de publicação do livro",
   })
   publishYear: number;
 
   @IsInt()
   @Min(0)
   @ApiProperty({
-    description: ' Quantidade de exemplares do livro',
+    description: " Quantidade de exemplares do livro",
   })
   quantity: number;
 
-  @IsString()
+  @IsOptional()
   @IsNotEmpty()
   @ApiProperty({
-    description: ' Autor do livro',
+    description: " Id do autor do livro",
+    required: false,
   })
-  authorId: string;
+  authorId?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({
+    description: " Nome do autor do livro",
+    required: false,
+  })
+  authorName?: string;
 }
