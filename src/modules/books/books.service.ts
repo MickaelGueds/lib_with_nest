be2e7c4c708a82fa4381ae8e991cc9929ef5baaa
Mickael <mickaelgoliveira@gmail.com>
2025-04-25 10:32:02 -0300

    fix: eslint and prettier, with correction bugs in books service

--- src/modules/books/books.service.ts
+++ src/modules/books/books.service.ts
@@ -1,39 +1,43 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma.service";
-import { CreateBookDto } from "./dto/create-book.dto";
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+
+import { PrismaService } from 'src/prisma/prisma.service';
+
+import { CreateBookDto } from './dto/create-book.dto';
 
 @Injectable()
 export class BooksService {
   constructor(private prisma: PrismaService) {}
 
+  // Create book method
   async create(createBookDto: CreateBookDto) {
-    let authorId: CreateBookDto.authorId;
-
-    if (!authorId && createBookDto.authorName) {
-      const author = await this.prisma.author.findFirst({
-        where: {
-          name: createBookDto.authorName,
-        },
-      });
-      if (!author) {
-        throw new NotFoundException("Autor não encontrado");
+    let authorId = createBookDto.authorId;
+      if (!authorId && createBookDto.authorName) {
+        const author = await this.prisma.author.findFirst({
+          where: {
+            name: createBookDto.authorName,
+          },//if id not show, so search for name, and if not name return exception 
+        });
+        if (!author) {
+          throw new NotFoundException('Autor não encontrado');
+        }
+        authorId = author.id;
       }
-      authorId = author.id;
-    }
-
-    if (!authorId && !createBookDto.authorId) {
-      throw new BadRequestException("Autor não encontrado");
+      if (!authorId && !createBookDto.authorId) {
+        throw new BadRequestException('Autor não encontrado');
     }
     const { authorName, ...bookData } = createBookDto;
-
     return this.prisma.book.create({
       data: {
         ...bookData,
         authorId,
       },
       include: {
-        author: true, // Inclui os dados do autor na resposta
+        author: true,
       },
     });
   }
+
+
+
+
 }
--- src/modules/books/dto/create-book.dto.ts
+++ src/modules/books/dto/create-book.dto.ts
@@ -1,39 +1,39 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsInt, IsNotEmpty, IsOptional, IsString, Min } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsNotEmpty, IsOptional, IsString, Min } from 'class-validator';
 
 export class CreateBookDto {
   @IsString()
   @IsNotEmpty()
   @ApiProperty({
-    description: " Titulo do livro",
+    description: ' Titulo do livro',
   })
   title: string;
 
   @IsString()
   @IsNotEmpty()
   @ApiProperty({
-    description: "ISBN do livro",
+    description: 'ISBN do livro',
   })
   isbn: string;
 
   @IsInt()
   @Min(1900)
   @ApiProperty({
-    description: " Ano de publicação do livro",
+    description: ' Ano de publicação do livro',
   })
   publishYear: number;
 
   @IsInt()
   @Min(0)
   @ApiProperty({
-    description: " Quantidade de exemplares do livro",
+    description: ' Quantidade de exemplares do livro',
   })
   quantity: number;
 
   @IsOptional()
   @IsNotEmpty()
   @ApiProperty({
-    description: " Id do autor do livro",
+    description: ' Id do autor do livro',
     required: false,
   })
   authorId?: string;
@@ -41,7 +41,7 @@ export class CreateBookDto {
   @IsOptional()
   @IsString()
   @ApiProperty({
-    description: " Nome do autor do livro",
+    description: ' Nome do autor do livro',
     required: false,
   })
   authorName?: string;
