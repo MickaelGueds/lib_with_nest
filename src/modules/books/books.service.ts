cb837271bf0e874f278ab1a50cbad35fa948b4f9
Mickael <mickaelgoliveira@gmail.com>
2025-04-28 12:00:28 -0300

    update: service with metod FindAll e FindOne

--- src/modules/books/books.service.ts
+++ src/modules/books/books.service.ts
@@ -3,6 +3,10 @@ import { BadRequestException, Injectable, NotFoundException } from '@nestjs/comm
 import { PrismaService } from 'src/prisma/prisma.service';
 
 import { CreateBookDto } from './dto/create-book.dto';
+import { skip } from 'node:test';
+import { contains } from 'class-validator';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { existsSync } from 'fs';
 
 @Injectable()
 export class BooksService {
@@ -36,6 +40,63 @@ export class BooksService {
       },
     });
   }
+
+  async findAll(params: { skip?: number; take?: number;  title?: string; authorName?: string }) {
+    const{skip = 0, take = 10, title, authorName} = params
+    return this.prisma.book.findMany({
+      skip,
+      take,
+      where: {
+        ...(title && {
+          title: { contains: title,mode: 'insensitive' }
+        }),
+        ...(authorName && {
+          authorName: {contains: authorName,mode: 'insensitive'}
+        })
+
+      },
+      include: { author: true },
+      orderBY: {createdAt: 'desc'}
+    })
+  }
+
+  async findOne(id: string ) {
+    const book = await this.prisma.book.findUnique({
+      where: { id },
+      include: {author: true}
+    })
+    if (!book) {
+      throw new NotFoundException('Livro nao encontrado')
+    }
+    return book
+  }
+
+  async update(id: string, updateBookDto: UpdateBookDto) {
+    const existingBook = await this.prisma.book.findUnique({
+      if (!existingBook) {
+        throw new NotFoundException('Livro nao encontrado') 
+      } 
+    })
+    let authorId = updateBookDto.authorId
+    
+    if (!authorId && updateBookDto.authorId) {
+      const author = await this.prisma.book.findFirst({
+        where: {name: updateBookDto.authorName}
+      })
+      if (!author) {
+        throw new NotFoundException('Autor não encotrado!')
+
+      }
+      authorId = author.id 
+    }
+    
+    
+  }
+
+
+
+  async remove()
+
 
 
 
